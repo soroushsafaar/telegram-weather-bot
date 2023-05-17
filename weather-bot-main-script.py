a4d55e0c23713240dc4cbdf18ccf20115ad405b0
soroushsafaar <abolfazl.saffarinia@gmail.com>
2023-05-17 16:56:14 +0330

    bot main script created

---
+++ weather-bot-main-script.py
@@ -0,0 +1,45 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
+import requests
+import json
+
+def start(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to your weather bot!")
+
+def weather(update: Update, context):
+    location = " ".join(context.args)
+    api_key = "44c609327b46265ed208bd55c16c2ea9"
+
+    # Make API call to get weather information
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+    response = requests.get(url)
+    weather_data = response.json()
+
+    print(json.dumps(weather_data, indent=4))  # Print the API response to the console
+
+    if weather_data["cod"] == 200:
+        # Extract relevant weather information from the response
+        temperature = weather_data["main"]["temp"]
+        description = weather_data["weather"][0]["description"]
+
+        # Send weather information to the user
+        message = f"Weather in {location}:\nTemperature: {temperature}K\nDescription: {description}"
+    else:
+        message = "Failed to retrieve weather information. Please try again."
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+def main():
+    bot_token = "6014284136:AAE4pk1ex8vexQCwuDtbSP_581bJZ3zVpEM"
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("weather", weather))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
